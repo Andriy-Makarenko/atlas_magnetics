429d08f549b4c86ceb2200d66f42d6e22115ad88
Andriy Makarenko <andriy@mate.academy>
2022-12-28 23:08:36 +0200

    Change flake8

--- pure_python.py
+++ pure_python.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-data_set = [364, 373, 358, 394, 378, 379, 357, 364, 350, 363, 392, 368, 359, 375, 399, 365, 379, 357, 380]
+data_set = [364, 373, 358, 394, 378, 379, 357, 364,
+            350, 363, 392, 368, 359, 375, 399, 365, 379, 357, 380]
 
 
 def data_validation(data: list):
@@ -55,14 +56,20 @@ def mode(data: list) -> list | str:
     for number in unique_values:
         count = data.count(number)  # count each number appearance in the data
         if count > 1:
-            result.append((number, count))  # add only numbers appeared more than once
+            # add only numbers appeared more than once
+            result.append((number, count))
     #  sort list tuples by count value
     result.sort(reverse=True, key=lambda item: item[1])
 
     if not result:  # if result is an empty list
         return "List doesn't have mode values"
-    # all value-count tuple pair with equal number of value appearance in the data
-    return [number_count_pair for number_count_pair in result if number_count_pair[1] == result[0][1]]
+    # all value-count tuple pair with equal number
+    # of value appearance in the data
+    return [
+        number_count_pair
+        for number_count_pair in result
+        if number_count_pair[1] == result[0][1]
+    ]
 
 
 def standard_deviation(data: list) -> list | str:
--- voltage_measuring.py
+++ voltage_measuring.py
@@ -11,11 +11,13 @@ void setup() {
     adc.begin();  # Initialize the ADC
     adc.setNPLC(nplc);  # set NPLC
     pinMode(potPin, INPUT);  # set working mode for port - Input data
-    Serial.begin(115200);  # set baud rate for data transfer - 14400 byte or 9600
+    # set baud rate for data transfer - 14400 byte or 9600
+    Serial.begin(115200);
 }
 
 void loop() {
-    potVal=analogRead(potPin);  # write measured values from potPin to potVal variable
+    # write measured values from potPin to potVal variable
+    potVal=analogRead(potPin);
     Serial.print(potVal); # send data from potVal to print from new line
     Serial.println(“,”);
     Serial.println(nplc);
@@ -25,23 +27,30 @@ void loop() {
 
 import time
 import serial
-arduinoData = serial.Serial("com3", "115200")  # creating an object to read data from.
+
+# creating an object to read data from.
 # With respective port and same baud rate we use in Arduino IDE
+arduinoData = serial.Serial("com3", "115200")
 
 time.sleep(1)  # set timer to give some time for com port to initialize
 max_binary = 1023  # max binary value from arduino ADC
 set_voltage = 3.3  # max voltage readings for our case
-voltage_conversion_coefficient = 3.3 / (1023 + 1)  # adding one as count starts from 0 
+# adding one as count starts from 0
+voltage_conversion_coefficient = 3.3 / (1023 + 1)
 
 while True:  # reading data from serial port
-    while arduinoData.in_waiting == 0:  # checking if there is any data on the port
+    # checking if there is any data on the port
+    while arduinoData.in_waiting == 0:
         pass
-    dataPacket = arduinoData.readline()  # reading data from port if there is some
+    # reading data from port if there is some
+    dataPacket = arduinoData.readline()
     dataPacket = str(dataPacket, "utf-8")  # get voltage binary readings
-    dataPacket = dataPacket.rstrip("\r\n")  # get rid of new lines we should delete carriage and \n
+    # get rid of new lines we should delete carriage and \n
+    dataPacket = dataPacket.rstrip("\r\n")
     dataPacket.split(",")  # get list of string values (voltage and NPLC)
     voltage_data = int(dataPacket[0])  # get binary voltage data
     NPLC_data = int(dataPacket[1])  # get NPLC settings
-    voltage = voltage_data * voltage_conversion_coefficient  # convert readings from binary to volts
+    # convert readings from binary to volts
+    voltage = voltage_data * voltage_conversion_coefficient
     voltage = round(voltage, 3)  # round answer to 3 digits after coma
     print(f"{voltage} v    NPLC-{NPLC_data}")  # output in convenient format
